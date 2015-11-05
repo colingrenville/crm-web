ae66c7110a02ba7161cd27ec1a3cd5fcae8cad6a
Colin Grenville <colingrenville0@gmail.com>
2015-11-05 15:07:50 -0500

    add show_contact

--- contact.rb
+++ contact.rb
@@ -1,3 +1,31 @@
+# class Contact
+ 
+# 	attr_accessor :first_name, :last_name, :email, :notes, :id
+
+# 	@@contacts = []
+# 	@@id = 1
+
+
+# 	def initialize(first_name, last_name, email, notes)
+# 		@id = @@id
+# 		@first_name = first_name
+# 		@last_name = last_name
+# 		@email = email
+# 		@notes = notes
+# 		@@id +=1 
+# 	end
+
+# 	def self.create(first_name, last_name, email, notes)
+# 		new_contact = Contact.new(first_name, last_name, email, notes)
+# 		@@contacts << new_contact
+# 	end
+
+# 	def self.all
+# 		@@contacts
+# 	end
+
+# end
+
 class Contact
  
 	attr_accessor :first_name, :last_name, :email, :notes, :id
@@ -18,10 +46,92 @@ class Contact
 	def self.create(first_name, last_name, email, notes)
 		new_contact = Contact.new(first_name, last_name, email, notes)
 		@@contacts << new_contact
+		new_contact
 	end
 
 	def self.all
 		@@contacts
+	end
+
+	def self.delete_all
+		@@contacts = [] 
+	end
+
+	def self.find(id_number)
+		@@contacts.find do |x|
+			if id_number = x.id
+				return x
+			end
+		end
+	end
+
+	def full_name
+		return "#{first_name} #{last_name}"
+	end
+
+	def update(attribute, newvalue)
+		@newvalue = newvalue
+		if attribute == "note"
+			change_note
+		elsif attribute == "first_name"
+			change_first_name
+		elsif attribute == "last_name"
+			change_last_name
+		elsif attribute == "email"
+			change_email
+		end
+	end
+
+	def change_note
+		notes = @newvalue
+		return notes
+	end
+
+	def change_first_name
+		first_name = @newvalue
+		return first_name
+	end
+
+	def change_last_name
+		last_name = @newvalue
+		return last_name
+	end
+
+	def change_email
+		email = @newvalue
+		return email
+	end
+
+	def self.search_by_attribute(attribute, value)
+		if attribute == "first_name"
+			@@contacts.each do |x|
+				if x.first_name == value
+					x
+				end
+			end
+		elsif attribute == "last_name"
+			@@contacts.each do |x|
+				if x.last_name == value
+					x
+				end
+			end
+		elsif attribute == "email"
+			@@contacts.each do |x|
+				if x.email == value
+					x
+				end
+			end
+		elsif attribute == "notes"
+			@@contacts.each do |x|
+				if x.notes == value
+					x
+				end
+			end
+		end
+	end
+	
+	def delete
+		@@contact = []
 	end
 
 end
--- crm.rb
+++ crm.rb
@@ -1,6 +1,8 @@
 require_relative 'contact.rb'
 require 'sinatra'
 
+# Temporary fake data so that we always find contact with id 1000.
+Contact.create("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar")
 
 get '/' do
 	@crm_app_name = "My CRM"
@@ -15,6 +17,10 @@ get '/contacts/new' do
 	erb :new_contact
 end
 
+get '/contacts/1000' do
+	@contact = Contact.find(1000)
+	erb :show_contact
+end
 
 post '/contacts' do
 	Contact.create(params[:first_name], params[:last_name], params[:email], params[:notes])
