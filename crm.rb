3f63bc1d77a4ca3afa993726b2489c1a040bc2ad
Colin Grenville <colingrenville0@gmail.com>
2015-11-07 09:31:41 -0500

    database working, options functional

--- crm.rb
+++ crm.rb
@@ -1,4 +1,4 @@
-require_relative 'contact.rb'
+# require_relative 'contact.rb'
 require 'sinatra'
 require 'data_mapper'
 
@@ -16,64 +16,71 @@ end
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
+
+post "/contacts" do
+  contact = Contact.create(
+    :first_name => params[:first_name],
+    :last_name => params[:last_name],
+    :email => params[:email],
+    :note => params[:note]
+  )
+  redirect to('/contacts')
+ end
 # Temporary fake data so that we always find contact with id 1000.
 
-# get '/' do
-# 	@crm_app_name = "My CRM"
-#   	erb :index
-# end
+get '/' do
+	@crm_app_name = "My CRM"
+  	erb :index
+end
 
-# get '/contacts' do
-#   	erb :contacts
-# end
+get '/contacts' do
+ @contacts = Contact.all
+  	erb :contacts
+end
 
-# get '/contacts/new' do
-# 	erb :new_contact
-# end
+get '/contacts/new' do
+	erb :new_contact
+end
 
-# get "/contacts/:id" do
-#   @contact = Contact.find(params[:id].to_i)
-#   if @contact
-#     erb :show_contact
-#   else
-#     raise Sinatra::NotFound
-#   end
-# end
+get "/contacts/:id" do
+  @contact = Contact.get(params[:id].to_i)
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
 
-# get '/contacts/:id/edit' do
-# 	@contact = Contact.find(params[:id].to_i)
-# 	if @contact 
-# 		erb :edit_contact
-# 	else
-# 		raise Sinatra::NotFound
-# 	end
-# end
+get '/contacts/:id/edit' do
+	@contact = Contact.get(params[:id].to_i)
+	if @contact 
+		erb :edit_contact
+	else
+		raise Sinatra::NotFound
+	end
+end
 
-# put '/contacts/:id' do
-# 	@contact = Contact.find(params[:id].to_i)
-#   	if @contact 
-# 	    @contact.first_name = params[:first_name]
-# 	    @contact.last_name = params[:last_name]
-# 	    @contact.email = params[:email]
-# 	    @contact.notes = params[:notes]
+put '/contacts/:id' do
+	@contact = Contact.get(params[:id].to_i)
+  	if @contact 
+	    @contact.first_name = params[:first_name]
+	    @contact.last_name = params[:last_name]
+	    @contact.email = params[:email]
+	    @contact.note = params[:note]
+	    @contact.save
+		redirect to("/contacts")
+	else
+		raise Sinatra::NotFound
+	end
+end
 
-# 		redirect to("/contacts")
-# 	else
-# 		raise Sinatra::NotFound
-# 	end
-# end
 
-# post '/contacts' do
-# 	Contact.create(params[:first_name], params[:last_name], params[:email], params[:notes])
-# 	redirect to('/contacts')
-# end
-
-# delete '/contacts/:id' do
-# 	@contact = Contact.find(params[:id].to_i)
-# 	if @contact
-# 		@contact.remove
-# 		redirect to ("/contacts")
-# 	else
-# 		raise Sinatra::NotFound
-# 	end
-# end
+delete '/contacts/:id' do
+	@contact = Contact.get(params[:id].to_i)
+	if @contact
+		@contact.destroy
+		redirect to ("/contacts")
+	else
+		raise Sinatra::NotFound
+	end
+end
